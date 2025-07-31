2b5844b5606125c2530463de4f3a47d10c6927cf
Lucas Ha <lucas@hasia.kr>
2025-07-31 13:03:58 +0900

    Deploy to GitHub Pages - Thu Jul 31 13:03:58 KST 2025

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/the-realty-itemlist-dashboard/sw.js', { scope: '/the-realty-itemlist-dashboard/' })})}
